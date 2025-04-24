7214eab90466efd4c17c852844c620a9d7cf25e7
Kisuu19 <kisurisam@gmail.com>
2025-04-24 13:43:15 +0530

    Added change range

---
+++ guessing_game.py
@@ -0,0 +1,8 @@
+import random 
+number = random.randint(1, 100) 
+print("Guess a number between 1 and 100") 
+guess = int(input()) 
+if guess == number: 
+print("You win!") 
+else: 
+print(f"Wrong! The number was {number}") 
